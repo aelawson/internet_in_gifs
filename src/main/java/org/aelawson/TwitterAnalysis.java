ce51e3be5cb56cb80ab764b26fbe4407afad2f6e
Andrew Lawson <alawson@jwplayer.com>
2018-04-10 08:57:52 -0400

    Use real twitter API instead of fake data. Import twitter API config. Fix logging.

--- src/main/java/org/aelawson/TwitterAnalysis.java
+++ src/main/java/org/aelawson/TwitterAnalysis.java
@@ -19,12 +19,17 @@
 package org.aelawson;
 
 import java.io.File;
+import java.util.Properties;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.api.common.functions.FlatMapFunction;
@@ -33,30 +38,37 @@ import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.util.Collector;
 
-import org.aelawson.util.TwitterExampleData;
 import org.aelawson.util.TokenizeTweet;
 
 public class TwitterAnalysis {
 
 	public static void main(String[] args) throws Exception {
+		PropertiesConfiguration twitterConfig = null;
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		final Logger LOG = LoggerFactory.getLogger(TwitterAnalysis.class);
+
 		Configurations configs = new Configurations();
 
 		try {
-		    PropertiesConfiguration twitterConfig = configs.properties(
+		    twitterConfig = configs.properties(
 						new File("src/main/resources/twitter.properties")
 		    );
 		}
 		catch (ConfigurationException e) {
-		    System.out.println("Error loading configuration...");
+		    LOG.info("Error loading configuration...");
 		    System.exit(1);
 		}
 
-		final ParameterTool params = ParameterTool.fromArgs(args);
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		env.getConfig().setGlobalJobParameters(params);
+		Properties twitterProps = new Properties();
+		twitterProps.setProperty(TwitterSource.CONSUMER_KEY, twitterConfig.getString("key"));
+		twitterProps.setProperty(TwitterSource.CONSUMER_SECRET, twitterConfig.getString("secret"));
+		twitterProps.setProperty(TwitterSource.TOKEN, twitterConfig.getString("token"));
+		twitterProps.setProperty(TwitterSource.TOKEN_SECRET, twitterConfig.getString("token_secret"));
 
-		System.out.println("Executing Twitter analysis with example data.");
-		DataStream<String> streamSource = env.fromElements(TwitterExampleData.TEXTS);
+		TwitterSource twitterSource = new TwitterSource(twitterProps);
+
+		LOG.info("Executing Twitter analysis with example data.");
+		DataStream<String> streamSource = env.addSource(twitterSource);
 
 		DataStream<Tuple2<String, Integer>> tweets = streamSource
 				.flatMap(new TokenizeTweet())
