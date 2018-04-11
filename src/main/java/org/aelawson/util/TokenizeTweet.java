5fa8e3e24dfa39bfb695ca38a65734eeecdad107
Andrew Lawson <alawson@jwplayer.com>
2018-04-11 00:04:17 -0400

    Use string splitting instead of older string tokenizer.

--- src/main/java/org/aelawson/util/TokenizeTweet.java
+++ src/main/java/org/aelawson/util/TokenizeTweet.java
@@ -1,7 +1,5 @@
 package org.aelawson.util;
 
-import java.util.StringTokenizer;
-
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
@@ -10,7 +8,6 @@ import org.apache.flink.util.Collector;
 
 public class TokenizeTweet implements FlatMapFunction<String, Tuple2<String, Integer>> {
   public static final long serialVersionUID = 1L;
-
   private transient ObjectMapper jsonParser;
 
   @Override
@@ -21,11 +18,10 @@ public class TokenizeTweet implements FlatMapFunction<String, Tuple2<String, Int
 
     JsonNode jsonNode = jsonParser.readValue(value, JsonNode.class);
     if (jsonNode.has("text")) {
-      StringTokenizer tokenizer = new StringTokenizer(jsonNode.get("text").asText());
-      while (tokenizer.hasMoreTokens()) {
-        String result = tokenizer.nextToken().replaceAll("\\s*", "").toLowerCase();
-        if (!result.equals("")) {
-          out.collect(new Tuple2<>(result, 1));
+      String[] tokens = jsonNode.get("text").asText().split("\\s");
+      for (String token : tokens) {
+        if (!token.equals("")) {
+          out.collect(new Tuple2<>(token, 1));
         }
       }
     }
